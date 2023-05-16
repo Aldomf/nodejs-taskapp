0bbc62c97275ee2dc954c901f141fce79eadea01
Aldo Miralles <alditomiralles3@gmail.com>
2023-05-17 00:16:27 +0200

    My first commit

---
+++ src/routes/index.routes.js
@@ -0,0 +1,26 @@
+import { Router } from "express";
+
+import {
+  renderTask,
+  createTask,
+  renderTaskEdit,
+  editTask,
+  deleteTask,
+  taskToggleDone,
+} from "../controllers/tasks.controllers";
+
+const router = Router();
+
+router.get("/", renderTask);
+
+router.post("/tasks/add", createTask);
+
+router.get("/edit/:id", renderTaskEdit);
+
+router.post("/edit/:id", editTask);
+
+router.get("/delete/:id", deleteTask);
+
+router.get("/toggleDone/:id", taskToggleDone);
+
+export default router;
